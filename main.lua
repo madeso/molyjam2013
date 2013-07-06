a36e1e36a0049fd5319f58cb0f2b4ea7987509af
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-06 14:14:45 +0200

    title screen

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+function Img(p)
+	local r
+	r = love.graphics.newImage(p)
+	return r
+end
+
+local Draw = love.graphics.draw
+
+local title = Img("title.png")
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    Draw(title, 0,0)
 end
