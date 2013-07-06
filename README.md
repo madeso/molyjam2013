93c465d3bb046383069ee8856f5edb427e500892
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-06 16:46:18 +0200

    added music and font

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ A Knightly BEATemup:
 
 A musicbased beatem up/fightingbased rythm game. You play a knight fighting monsters in on your way to save the princess. in the tower (obviosuly inspired by Rapunzel).
 
+Created by:
+Art(cc) & code(gpl) by Gustav Jansson
+Music by Robbie Dooley
+Public font, Alex-brush(OFL) by Rosette http://openfontlibrary.org/en/font/alex-brush
+
 Design document
 ================
 
--- main.lua
+++ main.lua
@@ -16,6 +16,36 @@ function Sfx(p)
 	return s
 end
 
+function to01(a,x,b)
+	return x-a/(b-a)
+end
+
+function from01(a,x,b)
+	return (b-a)*x + a
+end
+
+function wrap01(x)
+	local r
+	r = x
+	while r > 1 do
+		r = r - 1
+	end
+	while r < 0 do
+		r = r + 1
+	end
+	return r
+end
+
+-----------------------------------------------------------------------------------------
+-- Startup
+local state = 1
+
+function love.load()
+	love.graphics.setFont(love.graphics.newFont("AlexBrush-Regular-OTF.otf", 30))
+	Setup()
+end
+
+-----------------------------------------------------------------------------------------
 -- Aliases
 local Draw = love.graphics.draw
 local Play = love.audio.play
@@ -23,20 +53,14 @@ local PauseAllAudio = love.audio.pause
 
 -- Sounds
 local titlemusic = Music("title.ogg")
+local foresta = Music("foresta.ogg")
+local test = Sfx("test.ogg")
 
 -- Graphics
 local title = Img("title.png")
 local gamebkg = Img("world.png")
 local player = Img("player.png")
 local stone = Img("stone.png")
-
-local state = 1
-
-function love.load()
-	if state == 0 then
-		Play(titlemusic)
-	end
-end
 
 -----------------------------------------------------------------------------------------
 function title_onkey(key)
@@ -50,21 +74,41 @@ function title_draw()
 end
 function title_update(dt)
 end
+function title_setup()
+	Play(titlemusic)
+end
 
 
 -----------------------------------------------------------------------------------------
+
+function Stone()
+	local s
+	s = {}
+	s.pos = 600
+	return s
+end
+local stones = {}
 local jumptimer = -1
+local gametimer = 0
+
+local currentxp = 0
+local currentlevel = 1
+
 function game_draw()
 	local jumpheight = 0
 	if jumptimer > 0 then
-		jumpheight = 180*math.sin(3.14*jumptimer)
+		jumpheight = 210*math.sin(3.14*jumptimer)
 	else
 		jumpheight = 0
 	end
     Draw(gamebkg, 0,0)
 	Draw(player, 72,300 - jumpheight)
 	-- jumpheight less than 60 = player collide with stone & x between -10 and 160
-	Draw(stone, 200,340)
+	local stonepos = 0
+	stonepos = from01(-256, wrap01(gametimer - 0.8), 800)
+	Draw(stone, stonepos,340)
+	
+	love.graphics.printf("Experience: " .. currentxp .. " & Level: " .. currentlevel, 0, 0, 780, "right")
 end
 function game_onkey(key)
 	if key == " " then
@@ -74,17 +118,36 @@ function game_onkey(key)
 	end
 end
 function game_update(dt)
+	gametimer = gametimer - dt / 2
+	if gametimer < 0 then
+		gametimer = gametimer + 1
+		Play(test)
+	end
+	
 	if jumptimer > 0 then
-		jumptimer = jumptimer - dt/8
+		jumptimer = jumptimer - dt*2
 	else
 		jumptimer = -1
 	end
+end
+function game_setup()
+	Play(foresta)
 end
 
 
 
 
 -----------------------------------------------------------------------------------------
+function Setup()
+	if state == 0 then
+		tile_setup()
+	elseif state == 1 then
+		game_setup()
+	else
+		print("unknown gamestate " .. state)
+	end
+end
+
 function love.draw()
 	if state == 0 then title_draw()
 	elseif state == 1 then game_draw()
