44eec440beade11897b2b276133bda28b5c5d0de
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-06 15:00:50 +0200

    static stone with basic player sprite

--- main.lua
+++ main.lua
@@ -7,7 +7,20 @@ end
 local Draw = love.graphics.draw
 
 local title = Img("title.png")
+local gamebkg = Img("world.png")
+local player = Img("player.png")
+local stone = Img("stone.png")
+
+function title_draw()
+	Draw(title, 0,0)
+end
+
+function game_draw()
+    Draw(gamebkg, 0,0)
+	Draw(player, 72,300)
+	Draw(stone, 400,340)
+end
 
 function love.draw()
-    Draw(title, 0,0)
+    game_draw()
 end
