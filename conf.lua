62bf3cc637b8745f044b09030c0c03e14b495b14
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-06 15:13:58 +0200

    implemented title music from artist

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-    t.title = "sir Knight Beats"        -- The title of the window the game is in (string)
+    t.title = "sir Knightly Beats"        -- The title of the window the game is in (string)
     t.author = "imadesome & friends"        -- The author of the game (string)
     t.url = nil                 -- The website of the game (string)
     t.identity = nil            -- The name of the save directory (string)
--- main.lua
+++ main.lua
@@ -4,13 +4,43 @@ function Img(p)
 	return r
 end
 
-local Draw = love.graphics.draw
+function Music(p)
+	local s
+	s = love.audio.newSource(p, "stream")
+	return s
+end
 
+function Sfx(p)
+	local s
+	s = love.audio.newSource(p, "static")
+	return s
+end
+
+-- Aliases
+local Draw = love.graphics.draw
+local Play = love.audio.play
+local PauseAllAudio = love.audio.pause
+
+-- Sounds
+local titlemusic = Music("title.ogg")
+
+-- Graphics
 local title = Img("title.png")
 local gamebkg = Img("world.png")
 local player = Img("player.png")
 local stone = Img("stone.png")
 
+local state = 0
+
+Play(titlemusic)
+
+-----------------------------------------------------------------------------------------
+function title_onkey(key)
+	if key == " " then
+		PauseAllAudio()
+		state = 1
+	end
+end
 function title_draw()
 	Draw(title, 0,0)
 end
@@ -20,7 +50,25 @@ function game_draw()
 	Draw(player, 72,300)
 	Draw(stone, 400,340)
 end
+function game_onkey(key)
+end
 
 function love.draw()
-    game_draw()
+	if state == 0 then title_draw()
+	elseif state == 1 then game_draw()
+	else
+		love.graphics.print("unknown gamestate " .. state, 400, 300)
+	end
+end
+
+function love.keyreleased(key)
+   if key == "escape" then
+      love.event.push("quit")   -- actually causes the app to quit
+   end
+   
+   if state == 0 then title_onkey(key)
+	elseif state == 1 then game_onkey(key)
+	else
+		love.graphics.print("unknown gamestate " .. state, 400, 300)
+	end
 end
