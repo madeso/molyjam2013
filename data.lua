a4c9fc6983c70ba73d548c85172e78eb87d70c83
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-07 23:47:53 +0200

    more font tweaking

--- data.lua
+++ data.lua
@@ -1,19 +1,20 @@
 JUMPKEY = "up"
 SLIDEKEY = "down"
 SWORDKEY = "right"
-FORESTDATA = " ---ooo888rrr"
+FORESTDATA = "--"
 --FORESTDATA = "   o   o   o   o o oo- - - --ooo o o -o- - -o-o o o-o- - -o   8   8 - 8 o 8o 8- -o- o8o -8o o8- o8o8 -o-8o -8o -88 oo- 8-- oo 88 -- oo 8-o8 -o-8 8o-o8-8o-8o88oo--o8--8-oo-o8"
-CAVEDATA = " -o8-o8-o8rrr"
-HAUNTEDDATA = " -o88o--o8rrr"
+CAVEDATA = "-o8"
+HAUNTEDDATA = "-o8"
 MAXHEALTH = 9
 SPACEBETWEENHEARTS = 18
 HEARTX = 5
 HEARTY = 5
 XPLEVEL = 4
 
-STATX = 100
+STATX = 40
+STATXP = 420
 STATY = 150
-STATDIFF = 40
+STATDIFF = 45
 
 -- possible change this depending on world
 REACTIONTIME = 0.2
