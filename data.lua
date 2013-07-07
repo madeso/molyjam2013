925965bdebb34141010a61449c7bb206edc6a618
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-07 18:29:45 +0200

    new leveldata from designer

--- data.lua
+++ data.lua
@@ -1,7 +1,8 @@
 JUMPKEY = "up"
 SLIDEKEY = "down"
 SWORDKEY = "right"
-FORESTDATA = " ---ooo888rrr"
+--FORESTDATA = " ---ooo888rrr"
+FORESTDATA = "   o   o   o   o o oo- - - --ooo o o -o- - -o-o o o-o- - -o   8   8 - 8 o 8o 8- -o- o8o -8o o8- o8o8 -o-8o -8o -88 oo- 8-- oo 88 -- oo 8-o8 -o-8 8o-o8-8o-8o88oo--o8--8-oo-o8"
 CAVEDATA = " -o8-o8-o8rrr"
 HAUNTEDDATA = " -o88o--o8rrr"
 MAXHEALTH = 9
