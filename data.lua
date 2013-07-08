94938798068fa2b03930a27ab424596cc4160876
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-08 02:29:39 +0200

    level data from designer

--- data.lua
+++ data.lua
@@ -1,15 +1,17 @@
 JUMPKEY = "up"
 SLIDEKEY = "down"
 SWORDKEY = "right"
-FORESTDATA = "rrrrrrrrrrrrrrrrrrrrr"
---FORESTDATA = "   o   o   o   o o oo- - - --ooo o o -o- - -o-o o o-o- - -o   8   8 - 8 o 8o 8- -o- o8o -8o o8- o8o8 -o-8o -8o -88 oo- 8-- oo 88 -- oo 8-o8 -o-8 8o-o8-8o-8o88oo--o8--8-oo-o8"
-CAVEDATA = "rrrrrrrrrrrrrrrrrrrrrrr"
-HAUNTEDDATA = "rrrrrrrrrrrrrrrr"
+
+
+FORESTDATA = " o o -o o- -8 8 o-8 -8o o8-o8-888"
+CAVEDATA = " - o 8- o- 8-8 oo -- 888-o8-o8-88o-8"
+HAUNTEDDATA = " o o-8- rr-8rro rr-rr-r ro8-o8ro 8o- ro- -o8r -o8r r8o- -o8rrr r"
+
 MAXHEALTH = 6
 SPACEBETWEENHEARTS = 36
 HEARTX = 5
 HEARTY = 5
-XPLEVEL = 12
+XPLEVEL = 7
 ANIMSPEED = 0.04
 
 STATX = 40
