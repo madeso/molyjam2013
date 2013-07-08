2903807b4d264b1a045e9d4665c082eb68dddfd6
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-08 03:51:01 +0200

    tweaking

--- data.lua
+++ data.lua
@@ -7,7 +7,7 @@ FORESTDATA = " o o -o o- -8 8 o-8 -8o o8-o8-888"
 CAVEDATA = " - o 8- o- 8-8 oo -- 888-o8-o8-88o-8"
 HAUNTEDDATA = " o o-8- rr-8rro rr-rr-r ro8-o8ro 8o- ro- -o8r -o8r r8o- -o8rrr r"
 
-MAXHEALTH = 6
+MAXHEALTH = 5
 SPACEBETWEENHEARTS = 36
 HEARTX = 5
 HEARTY = 5
